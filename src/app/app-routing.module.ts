61a1983b06c1cefa603c2cbc70b8da7c27402372
Ariel Aharon <xarielah@gmail.com>
2025-05-26 23:41:30 +0300

    update routes and guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { authGuard } from './guards/auth.guard';
+import { notAuthdGuard } from './guards/not-authd.guard';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
+import { ProtectedComponent } from './pages/protected/protected.component';
 import { RegisterComponent } from './pages/register/register.component';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent, canActivate: [notAuthdGuard] },
+  {
+    path: 'register',
+    component: RegisterComponent,
+    canActivate: [notAuthdGuard],
+  },
+  {
+    path: 'protected',
+    component: ProtectedComponent,
+    canActivate: [authGuard],
+  },
   { path: '', component: HomeComponent, pathMatch: 'full' },
 ];
 
